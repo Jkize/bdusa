9903841a014e2a82757a826af7d1581af5073cd2
Labing <Labing@LabingXEON-PC>
2018-10-26 12:37:39 -0500

    v0.1

--- src/main/java/edu/co/sergio/mundo/dao/Conexion.java
+++ src/main/java/edu/co/sergio/mundo/dao/Conexion.java
@@ -9,22 +9,14 @@ public class Conexion {
     private static Connection CONEXION = null;
 
     public static Connection getConnection() throws URISyntaxException {
-        String HOST = "ec2-54-225-115-234.compute-1.amazonaws.com:5432";
-        String DATABASE = "d9ulg2a1cejvqd";
-        String USER = "anhiwafvnumrrj";
-        String PASS = "080af5acdae363cfb03de724c7d8bde18897e847113cc0cb5367bfff438dfdbd";
-
-        URI dbUri = new URI(System.getenv("postgres://anhiwafvnumrrj:080af5acdae363cfb03de724c7d8bde18897e847113cc0cb5367bfff438dfdbd@ec2-54-225-115-234.compute-1.amazonaws.com:5432/d9ulg2a1cejvqd"));
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://"
-                + HOST + "/" + DATABASE
-                + "?user=" + USER + "&password="
-                + PASS + "&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
         if (CONEXION == null) {
             try {
-                CONEXION = DriverManager.getConnection(dbUrl);
+                CONEXION = DriverManager.getConnection(dbUrl, username, password);
             } catch (SQLException e) {
                 System.out.println("Connection Failed! Check output console");
                 e.printStackTrace();
